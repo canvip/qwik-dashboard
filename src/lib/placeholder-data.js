934d00fc3389e685d37ce01bb11a3ce354603feb
Ahmed Alaa Abd Alhy <canvip2011@gmail.com>
2025-01-13 11:35:49 +0200

    Update placeholder-data.js

--- src/lib/placeholder-data.js
+++ src/lib/placeholder-data.js
@@ -1,4 +1,15 @@
-SELECT invoices.amount, customers.name
-FROM invoices
-JOIN customers ON invoices.customer_id = customers.id
-WHERE invoices.amount = 666;
+const invoices = [
+  {
+    customer_id: customers[0].id,
+    amount: 15795,
+    status: 'pending',
+    date: '2022-12-06',
+  },
+  {
+    customer_id: customers[1].id,
+    amount: 20348,
+    status: 'pending',
+    date: '2022-11-14',
+  },
+  // ...
+];
